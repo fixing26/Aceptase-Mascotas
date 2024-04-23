0a95bcad4e67774821970c03f7cf51dad6589d49
fixing26 <jsebastianposada@gmail.com>
2024-04-23 19:54:47 +0200

    modified:   public/index.html 	modified:   public/routes/Anuncio.js

--- public/routes/Anuncio.js
+++ public/routes/Anuncio.js
@@ -7,6 +7,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const AnuncioModel = require("../schemas/Anuncio-schema");
+const { default: mongoose, Schema } = require("mongoose");
 
 AnuncioRouter.post("/NuevoAnuncio", async (req,res)=>{
 
@@ -20,6 +21,27 @@ AnuncioRouter.post("/NuevoAnuncio", async (req,res)=>{
     await newAnuncio.save();
 
     return res.send("Anuncio publicado");
+});
+
+AnuncioRouter.get("/busqueda",async (req,res)=>{
+
+    try {
+    const {ciudad} = req.query;
+
+    if(!ciudad){
+        return res.status(400).json({error:'No se ha proporcionado la ciudad'});
+    }
+
+    const Resultado = await AnuncioModel.find({Ciudad:ciudad});
+    res.json(Resultado);
+   
+
+} catch (error) {
+    // Si ocurre algún error durante la ejecución, lo manejamos aquí
+    console.error("Error en la consulta:", error);
+    return res.status(500).json({ error: "Ocurrió un error al procesar la solicitud" });
+}
+
 });
 
 module.exports = AnuncioRouter;
